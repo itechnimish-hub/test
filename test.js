cd9cccbbc01b25474842893d0e478659c327b619
itechnimish-hub <itech@gmail.com>
2025-11-18 17:50:38 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+let firstname = ["suresh", "rakesh", "omkar"];
+
+let person = { name: "sanket", email: "a@gmil.com" };
+
+let check = typeof firstname;
+// firstname = "suresh";
+
+document.write(check);
+
+
+// document.getElementById('demo').innerHTML = firstname;
+
+// console.log("testing")
+
+
+
+
+// alert(firstname);
